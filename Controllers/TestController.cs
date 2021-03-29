86a259221f92d07dbbd6c7ac93a666b3cb2ccce4
chinmay dey <chinmay.dey@outlook.com>
2021-03-29 23:27:11 +1100

    Initial commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Net.Http.Headers;
+
+namespace NETCORE.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] 
+    public class TestController : ControllerBase
+    {      
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public TestController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet]
+        private async Task<string> GetToken()
+        {
+             var auth0Client = new HttpClient();
+                string token = "";
+                var bodyString = $@"{{""client_id"":""7oltoQo9D9MLwG51mkUFwDrApqotjNFz"", ""client_secret"":""PFQU7jntwOcG4yqeafPy8Y75x1EcMti85wrWEtXWnIjpadZQ4VATM3ls2ZxkLC9e"", ""audience"":""http://localhost:5000"", ""grant_type"":""client_credentials""}}";
+                var response = await auth0Client.PostAsync("https://dev-y2mwzdf7.au.auth0.com/oauth/token", new StringContent(bodyString, Encoding.UTF8, "application/json"));
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    var responseJson = JObject.Parse(responseString);
+                    token = (string)responseJson["access_token"];
+
+                }
+
+                return token;
+        }
+
+        [HttpGet]
+        [Route("GetData")]
+        public async Task<string> GetData()
+        {
+            var token = await GetToken();
+            var client = new HttpClient();
+
+            Console.Write(token);
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://localhost:5001/WeatherForecast");
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var booksResponse = await client.SendAsync(requestMessage);
+
+            var bookResponseString = await booksResponse.Content.ReadAsStringAsync();
+            var bookResponseJson = JArray.Parse(bookResponseString);
+
+            Console.Write(bookResponseJson);
+            
+            return bookResponseJson.ToString();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace NETCORE
+{
+    public class Startup
+    {
+        readonly string MyAllowSpecificOrigins = "AllowOrigins";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Console.Write(Configuration["Auth0:Authority"]);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                    builder => builder.AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .SetIsOriginAllowed(origin => true) // allow any origin
+                    .AllowCredentials());
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["Auth0:Authority"];
+                options.Audience = Configuration["Auth0:Audience"];
+            });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();           
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
